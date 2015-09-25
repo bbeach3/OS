609e8aa2c5971d4b7e91cdd7f5ea3293c1c66a46
jmrobinson93 <jmrobinson93@localhost>
2015-09-25 13:46:11 -0400

    Maybe possibly there's a chance I  could have fixed some things in menu.c

--- include/core/shutdown.c
+++ include/core/shutdown.c
@@ -10,13 +10,13 @@
 \Parameters: none
 \Returns: none
 */
-shutdown()
+void shutdown()
 {
 serial_println("Are you sure you want to shut down?");
 int size = 3;
 char *input[size];
 takeInput(*input,size);
-if(*input[0]=='y')
+//if(*input[0]=='y')
 //return to kmain()
 
 }
