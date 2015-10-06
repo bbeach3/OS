89fac9aaf4946ceaa41edc514e862bfe1bc8dcfd
amstewartwvu <amstewartwvu@localhost>
2015-10-06 10:59:58 -0400

    Added basic framework for PCBs/Quenes

---
+++ modules/mod2/pcb.c
@@ -0,0 +1,32 @@
+#include "pcb.h"
+#include "quene.h"
+
+pcb *allocatePCB()
+{
+	
+}
+
+int freePCB(pcb *oldpcb)
+{
+	
+}
+
+pcb *setupPCB(char *pcbname, unsigned int pcbproc, int pcbprior)
+{
+	
+}
+
+pcb *findPCB(char *pcbname)
+{
+	
+}
+
+void insertPCB(pcb *newpcb)
+{
+	
+}
+
+int removePCB(pcb *oldpcb)
+{
+	
+}
