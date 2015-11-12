d2e80d3f2c64f900146c06f8bfccae611dcd1d4c
Bradey Beach <bbeach3@mix.wvu.edu>
2015-11-12 00:29:29 -0500

    Made a working external mcb.c. Current functions - initialization, allocation, and isEmpty. We still need to make the rest.

--- modules/mod5/mcb.h
+++ modules/mod5/mcb.h
@@ -1,30 +1,46 @@
 #ifndef _MCB_H
 #define _MCB_H
 
-#include "mem/heap.h"
+typedef struct compmcb compmcb;
 
-typedef struct {
+struct compmcb{
 	int alloc; //0 is free, 1 is allocated
-	void *address;
+	void *address; //own (the compmcb's) address
 	int size;
-	void *next;
-	void *prev;
-} compmcb;
+	char pcbname[9];
+	compmcb *next;
+	compmcb *prev;
+};
 
-typedef struxt {
+typedef struct limitmcb limitmcb;
+
+struct limitmcb{
 	int alloc;
-	int size;	
-} limitmcb;
+	int size;	//of blocks + memory
+};
 
-struct heap *mcbheap;
-void *freelist;
-void *alloclist;
+typedef struct mcblist mcblist;
+
+struct mcblist{
+	compmcb *head;
+};
+
+extern mcblist* freelist;
+extern mcblist* alloclist;
+extern void* mcbheap;
 
 int initializeHeap();
 
-u32int *allocateMem(int bytesalloc);
+//prototype must match sys_alloc_mem when we replace mpx's with our own
+void *allocateMem(int bytesalloc);
+//for now, it doesn't, because I had some compiling issues.
+//when we make the switch, change the name, type of size...
+//if all else fails, just leave the name as is and have sys_alloc_mem call it		
 
-void freeMem();
+void insertMCB(compmcb *toInsert);
+
+//see notes above on allocateMem
+int freeMem(void *ptr);
 
 void showAllocMap();
 
